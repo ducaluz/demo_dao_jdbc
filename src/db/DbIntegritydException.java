7b97744ec18f24e94841c31411ab790a5fe611ad
ducaluz <duca.luz@gmail.com>
2024-02-14 12:52:15 -0300

    Preparacao do projeto

---
+++ src/db/DbIntegritydException.java
@@ -0,0 +1,11 @@
+package db;
+
+public class DbIntegritydException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DbIntegritydException(String msg) {
+		super(msg);
+	}
+
+}
